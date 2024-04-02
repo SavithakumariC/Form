504be08c2efb697a2585605b9d3cf9137b2b770b
SavithakumariC <savithakumari0918@gmail.com>
2024-04-02 21:08:54 +0530

    commit successfully

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Validation from "./Validation";
+import Validation from "./FormValidation/Validation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
