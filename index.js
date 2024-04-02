a28476913b4d295a480e023b198b4da81968a90a
SavithakumariC <savithakumari0918@gmail.com>
2024-04-02 19:38:36 +0530

    commit successfully

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import Validation from "./Hooks Form/Validation";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Validation />
+  </React.StrictMode>
+);
+reportWebVitals();
